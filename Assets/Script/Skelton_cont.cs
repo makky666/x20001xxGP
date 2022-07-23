f8c8622af193d69e82b13fa188ef397d950f8ad1
makky666 <86936201+makky666@users.noreply.github.com>
2022-07-23 17:16:29 +0900

    effect

--- Assets/Script/Skelton_cont.cs
+++ Assets/Script/Skelton_cont.cs
@@ -6,6 +6,8 @@ public class Skelton_cont : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody rb;
+    public GameObject breakEffect;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,16 @@ public class Skelton_cont : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
        Destroy(gameObject);
+        GenerateEffect();
+
+    }
+
+    //エフェクトを生成する
+    void GenerateEffect()
+    {
+        //エフェクトを生成する
+        GameObject effect = Instantiate(breakEffect) as GameObject;
+        //エフェクトが発生する場所を決定する(敵オブジェクトの場所)
+        effect.transform.position = gameObject.transform.position;
     }
 }
